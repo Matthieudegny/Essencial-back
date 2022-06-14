eb0d52732b6082e11371935299e188917cb4c9a3
Arthur <93485401+ArthurLebas@users.noreply.github.com>
2022-06-14 11:19:53 +0200

    fix connection db heroku

--- app/db/pg.js
+++ app/db/pg.js
@@ -14,7 +14,7 @@
  const debug = require('debug')('SQL:log');
  const { Pool } = require('pg');
  
- const pool = new Pool();
+ const pool = new Pool(process.env.DATABASE_URL);
  
  let queryCount = 0;
  
