e9e1d8321f18c075e38db86c13e770c955843838
Arthur <93485401+ArthurLebas@users.noreply.github.com>
2022-06-14 19:20:41 +0200

    test param jsdoc 3

--- app/routers/api/index.js
+++ app/routers/api/index.js
@@ -21,7 +21,7 @@ router.route('/user')
     /**
      * POST /api/user
      * @summary To get one user with his photo
-     * @param  {number} id.body - user id
+     * @param  {object} user - user object with only id
      * @return {object} 200 - success response
      * @return {object} 400 - input data invalid
     */
