461c90d73605c45917713d32bc7c33e0957b4509
Arthur <93485401+ArthurLebas@users.noreply.github.com>
2022-06-15 12:30:58 +0200

    fix param swagger

--- app/routers/api/index.js
+++ app/routers/api/index.js
@@ -21,7 +21,7 @@ router.route('/user')
     /**
      * POST /api/user
      * @summary To get one user with his photo
-     * @arg {object} request.body.required - user object with only id
+     * @arg {number} id.query.required - user object with only id
      * @return {object} 200 - success response
      * @return {object} 400 - input data invalid
     */
