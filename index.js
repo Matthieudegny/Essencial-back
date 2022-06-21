28e47ebee9f028890669e80f521590aef6ca6f8e
Arthur <93485401+ArthurLebas@users.noreply.github.com>
2022-06-21 13:57:52 +0200

    fix swagger config6

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const options = {
     paths: {
         "/api/user": {
             parameters: [ { name: "Authorization" , in: "header", type: "string", description: "auth token"}],
-            "post": {
+            "patch": {
                 "security": [ { "JWT": [] } ],
                 "tags": [
                     "User"
