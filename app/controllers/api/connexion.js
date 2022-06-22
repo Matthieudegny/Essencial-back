10d371db98aa445b14120ab2492f325f7bdc6555
Arthur <93485401+ArthurLebas@users.noreply.github.com>
2022-06-22 18:55:03 +0200

    add type summury type swagger5

--- app/controllers/api/connexion.js
+++ app/controllers/api/connexion.js
@@ -7,7 +7,7 @@ const bcrypt = require('bcrypt');
 /**
  * @typedef {object} Connexion
  * @summary Object to send to connect a user or an ecovillage
- * @property {string} email - Identifier unique primary key of the table
+ * @property {string} email - Email
  * @property {string} password - Password 
  */
 
