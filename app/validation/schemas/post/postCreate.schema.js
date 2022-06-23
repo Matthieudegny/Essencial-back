25541d3d67429b0e1e2abead4a47a7b0081fc0bc
Arthur <93485401+ArthurLebas@users.noreply.github.com>
2022-06-23 17:53:20 +0200

    add validation on create post2

--- app/validation/schemas/post/postCreate.schema.js
+++ app/validation/schemas/post/postCreate.schema.js
@@ -3,8 +3,6 @@ const Joi = require('joi');
 const textRule = Joi.string().min(2).pattern(/^[a-zA-ZÀ-ÿ0-9 ']+$/);
 
 module.exports = Joi.object({
-    user_id: Joi.number()
-        .required(),
     content: textRule
         .required(),
     title: textRule
