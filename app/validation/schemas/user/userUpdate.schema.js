e123edadc0cd858fd93b0ec2abb7a8ba2e8b7f83
Arthur <93485401+ArthurLebas@users.noreply.github.com>
2022-06-24 17:58:57 +0200

    remove joi validation from patch ev & user2

--- app/validation/schemas/user/userUpdate.schema.js
+++ app/validation/schemas/user/userUpdate.schema.js
@@ -6,9 +6,9 @@ module.exports = Joi.object({
     email: Joi.string()
     .pattern(/^[a-zA-Z0-9._-]{1,64}@([a-zA-Z0-9-]{2,252}\.[a-zA-Z.]{2,6})$/)
         .allow(null),
-    first_name: textRule
+    first_name: Joi.string()
         .allow(null),
-    last_name: textRule
+    last_name: Joi.string()
         .allow(null),
     pseudo: textRule
         .allow(null),
@@ -25,13 +25,13 @@ module.exports = Joi.object({
     phone_number: Joi.string()
         .pattern(/^(0|\+33)[ .-]?[1-9]([ .-]?[0-9]){8}$/)
         .allow(null),
-    address: textRule
+    address: Joi.string()
         .allow(null),
-    region: textRule
+    region: Joi.string()
         .allow(null),
     zip_code: textRule
         .allow(null),
-    city: textRule
+    city: Joi.string()
         .allow(null),
     path: Joi.string()
         .allow(null)
